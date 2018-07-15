039bc069ee7c0c9106fe69937a12bf59efbf8600
Joe Ren <earthengine@gmail.com>
2018-07-15 17:30:33 +1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+class Square extends React.Component {
+    render() {
+      var src = this.props.value % 2 === 1 ?  'h.svg' : 'o.svg';
+      var alt = this.props.value % 2 === 1 ?  'h' : 'o';
+      return (
+        <button className="square">
+          <img src={ src } alt={ alt }/>
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i}/>;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="infobar"/>
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
